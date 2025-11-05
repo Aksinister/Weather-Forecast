6950f5e12cf909f1a867bde9399a80bd7448f482
Aksinister <91866119+Aksinister@users.noreply.github.com>
2025-11-05 11:12:42 +1000

    Injected real weather forecast data into the SPA
    I made a function that utilised the data sent by axios from the forecast API and looped through the response for each of the seven days and injected the maximum and minimum temperature of that day into the HTML structure in the displayForecast function.
    
    to limit the days to just 5, I made the function for displayForecast only loop through up to the 5th day in the index (0-4).
    
    Inside of the displayForecast function I also called a function that would format the day to the name of a day of the week instead of an index number. Inside of the function I parsed the timestamp and then assigned the given number a name with the array of days of the week within the formatting function.

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-function manageSearchRequest(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input-bar");
-
-  searchCity(searchInput.value);
-}
-
-function searchCity(city) {
-  let apiKey = "7300c6775obt0415fe6635cd0da0d3fe";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  axios.get(apiUrl).then(updateWeatherStats);
-}
-
 function updateWeatherStats(response) {
   let temperatureItem = document.querySelector("#temperature-item");
   let temp = response.data.temperature.current;
@@ -21,7 +8,6 @@ function updateWeatherStats(response) {
   let timeItem = document.querySelector(`#time`);
   let emojiItem = document.querySelector("#emoji");
 
-  console.log(response.data);
   let date = new Date(response.data.time * 1000);
 
   temperatureItem.innerHTML = Math.round(temp);
@@ -32,48 +18,90 @@ function updateWeatherStats(response) {
   timeItem.innerHTML = formatDate(date);
   emojiItem.innerHTML = `<img src="${response.data.condition.icon_url}"/>`;
 
-  function formatDate(date) {
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let day = days[date.getDay()];
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-
-    return `${day} ${hours}:${minutes}`;
-  }
+  getForecast(response.data.city);
 }
 
-function displayForecast() {
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+function searchCity(city) {
+  let apiKey = "7300c6775obt0415fe6635cd0da0d3fe";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherStats);
+}
+function manageSearchRequest(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input-bar");
+
+  searchCity(searchInput.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "7300c6775obt0415fe6635cd0da0d3fe";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+
   let forecastItem = document.querySelector("#forecast");
-
   let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
-  <div id="monday" class="day">
-            <div class="forecast-emoji">☀️</div>
-            <div class="forecast-temperatures">
-              <div class="forecast-temperature"><strong>34°C</strong></div>
-              <div class="forecast-temperature">34°C</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+  <div class="day">
+            <div>
+              <img src="${day.condition.icon_url}" class="forecast-emoji"/>
             </div>
-            <div class="day-text">${day}</div>
+            <div class="forecast-temperatures">
+              <div class="forecast-temperature"><strong>${Math.round(
+                day.temperature.maximum
+              )}°C</strong></div>
+              <div class="forecast-temperature">${Math.round(
+                day.temperature.minimum
+              )}°C</div>
+            </div>
+            <div class="day-text">${formatDay(day.time)}</div>
           </div>
   `;
+    }
   });
 
   forecastItem.innerHTML = forecastHtml;
@@ -83,4 +111,3 @@ let searchFormItem = document.querySelector("#search-form");
 searchFormItem = addEventListener("submit", manageSearchRequest);
 
 searchCity("Brisbane");
-displayForecast();
